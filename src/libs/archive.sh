0d8a987f8809fb7db7f3923ef81a17f84636bcd4
范文华 <531362022@qq.com>
2019-09-05 15:38:26 +0800

    fix: Unable to archive in rn-0.60

--- src/libs/archive.sh
+++ src/libs/archive.sh
@@ -1,19 +1,40 @@
 #!/usr/bin/env bash
 
-mkdir -p ios/build
-
-rm -rf ios/build/archive.xcarchive
-
-project_name=$(ls ios | grep '.xcodeproj' | cut -d. -f1)
-
 # Archive
 # The same as Xcode -> Product -> Archive
-xcodebuild clean \
-  -project ./ios/${project_name}.xcodeproj \
-  -scheme ${project_name}
 
-xcodebuild archive \
-  -project ./ios/${project_name}.xcodeproj \
-  -scheme ${project_name} \
-  -configuration Release \
-  -archivePath ./ios/build/archive.xcarchive
+mkdir -p ios/build
+rm -rf ios/build/archive.xcarchive
+
+# Since RN-0.60，Pods is required and we can only use workspace to archive app
+# Rememver: .xcodeproj dirtory is useless once .xcworkspace dirtory exists
+workspace=$(ls ios | grep '.xcworkspace')
+archive_path=./ios/build/archive.xcarchive
+
+if [ -n "$workspace" ]
+then
+  project_name=$(echo $workspace | cut -d. -f1)
+
+  xcodebuild clean \
+    -workspace "./ios/$workspace" \
+    -scheme "$project_name"
+
+  xcodebuild archive \
+    -workspace "./ios/$workspace" \
+    -scheme "$project_name" \
+    -configuration "Release" \
+    -archivePath "$archive_path"
+else
+  project=$(ls ios | grep '.xcodeproj')
+  project_name=$(echo $project | cut -d. -f1)
+
+  xcodebuild clean \
+    -project "./ios/$project" \
+    -scheme ${project_name}
+
+  xcodebuild archive \
+    -project "./ios/$project" \
+    -scheme "$project_name" \
+    -configuration "Release" \
+    -archivePath "$archive_path"
+fi
