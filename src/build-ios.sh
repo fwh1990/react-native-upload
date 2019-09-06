5c56d6e22dca50a7c0f3d1ca65fe9d65f1c05cd7
范文华 <531362022@qq.com>
2019-09-07 00:50:12 +0800

    fix: Correct ios path

--- src/build-ios.sh
+++ src/build-ios.sh
@@ -17,4 +17,4 @@ echo -e "\n\033[32mBuilding ios app...\033[0m\n"
 sh $libs/archive.sh
 sh $libs/export-ipa.sh $export_method
 
-echo -e "\nView ipa file at: \033[32m./ios/ipa-$export_method\033[0m\n"
+echo -e "\nView ipa file at: \033[32m./ios/build/ipa-$export_method\033[0m\n"
--- src/build.sh
+++ src/build.sh
@@ -22,5 +22,5 @@ echo -e "\n\033[32mBuilding ios app...\033[0m\n"
 sh $libs/archive.sh
 sh $libs/export-ipa.sh $export_method
 
-echo -e "\nView apk file at: \033[32m./android/app/build/outputs/apk/release\033[0m\n"
-echo -e "\nView ipa file at: \033[32m./ios/ipa-$export_method\033[0m\n"
+echo -e "\nView apk file at: \033[32m./android/app/build/outputs/apk/release\033[0m"
+echo -e "\nView ipa file at: \033[32m./ios/build/ipa-$export_method\033[0m\n"
